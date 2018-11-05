567b4adf18049fe5d7a38e0a69a5d21462ffaee5
Velkatta <Velkatta@users.noreply.github.com>
2018-11-05 21:40:18 +0200

    Add files via upload

---
+++ MultyTwo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _10._Multiplication_Table_Two
+{
+    class MultyTwo
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine($"{first} X {second} = {first * second}");
+                second++;
+            }
+            while (second <= 10);
+        }
+    }
+}
