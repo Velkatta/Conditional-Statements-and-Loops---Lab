33071fe3c4530e62eb72328da2e55cb6316ceaca
Velkatta <Velkatta@users.noreply.github.com>
2018-11-05 21:37:20 +0200

    Add files via upload

---
+++ ForLanguages.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05._Foreign_Languages
+{
+    class ForLanguages
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+
+            switch (country)
+            {
+                case "USA":
+                case "England":
+                    Console.WriteLine("English");
+                    break;
+                case "Mexico":
+                case "Argentina":
+                case "Spain":
+                    Console.WriteLine("Spanish");
+                    break;
+
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
