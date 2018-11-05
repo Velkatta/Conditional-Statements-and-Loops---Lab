a28934ede06ab40a30f9e2b05cf25aad4b16bd00
Velkatta <Velkatta@users.noreply.github.com>
2018-11-05 21:39:57 +0200

    Add files via upload

---
+++ MultiplicationTable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _09._Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int count = 1;
+
+            while (count <= 10)
+            {
+                Console.WriteLine($"{n} X {count} = {n * count}");
+                count++;
+            }
+        }
+    }
+}
