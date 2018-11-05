829fce6e577632b536fca600eb5333d39f175b36
Velkatta <Velkatta@users.noreply.github.com>
2018-11-05 21:35:50 +0200

    Add files via upload

---
+++ PassedOrFailed.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02._Passed_or_Failed
+{
+    class PassedOrFailed
+    {
+        static void Main(string[] args)
+        {
+            double input = double.Parse(Console.ReadLine());
+
+            if (input >= 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+            else
+            {
+                Console.WriteLine("Failed!");
+            }
+        }
+    }
+}
