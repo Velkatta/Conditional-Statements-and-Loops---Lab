8417077724ce2cdd91302841c294cf449507694a
Velkatta <Velkatta@users.noreply.github.com>
2018-11-05 21:41:12 +0200

    Add files via upload

---
+++ NumberChecker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _12._Number_Checker
+{
+    class NumberChecker
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine("It is a number.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid input!"); 
+            }
+        }
+    }
+}
