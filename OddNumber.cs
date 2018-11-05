0f32e42c03bb5f2eea55f76bfe9b7b9bba98115d
Velkatta <Velkatta@users.noreply.github.com>
2018-11-05 21:40:46 +0200

    Add files via upload

---
+++ OddNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _11._Odd_Number
+{
+    class OddNumber
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int n = Math.Abs(int.Parse(Console.ReadLine()));
+                if (n % 2 == 0)
+                {
+                    Console.WriteLine($"Please write an odd number.");                  
+                }
+                if (n % 2 == 1)
+                {
+                    Console.WriteLine($"The number is: {n}");
+                    return;
+                }
+            }          
+        }
+    }
+}
