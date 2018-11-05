3a6f5575e536c1c2b7c8e5de4799631c405f4117
Velkatta <Velkatta@users.noreply.github.com>
2018-11-05 21:39:00 +0200

    Add files via upload

---
+++ SumOfOddNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _08._Sum_of_Odd_Numbers
+{
+    class SumOfOddNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+           
+            int sum = 0;
+            for (int i = 1; i <= n * 2; i+=2)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
