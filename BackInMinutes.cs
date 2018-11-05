7810bf9ed74d2bcd6817dab398a3a4556a3367d1
Velkatta <Velkatta@users.noreply.github.com>
2018-11-05 21:36:15 +0200

    Add files via upload

---
+++ BackInMinutes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _03._Back_in_30_Minutes
+{
+    class BackInMinutes
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 30;
+
+            if (minutes > 59)
+            {
+                hours++;
+                minutes -= 60;
+            }
+            if (hours > 23)
+            {
+                hours = 0;
+            }
+            if (hours <= 9)
+            {
+                Console.WriteLine($"{hours}:{minutes:d2}");
+                return;
+            }
+            Console.WriteLine($"{hours:d2}:{minutes:d2}");
+        }
+    }
+}
